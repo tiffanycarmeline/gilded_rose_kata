ac75e14a90968c49f5c3e918715e6d1ef83c1b73
Tiffany Silveira <tsilveira@cwhen-wks-44osx.changeworks.local>
2015-04-29 15:34:02 +0100

    indentation on sublime correction

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,79 +1,79 @@
   def update_quality(items)
-	 items.each do |item|
-	   find_updater(item)
-	 end
-	end
-
-	def find_updater(item)
-	 case item.name
-	   when "Backstage passes to a TAFKAL80ETC concert"
-			update_for_backstage(item)
-		 when "Aged Brie"
-		  update_for_aged_brie(item)
-		 when "Conjured Mana Cake"
-			update_for_cake(item)				
-		 when "Sulfuras, Hand of Ragnaros"
-			 #do nothing
-		 else
-			do_the_common_update(item)
-	 end
-	end
-
-	def do_the_common_update(item)
-	 qual = item.quality
-	 sell_in = item.sell_in - 1
-	  if qual > 0
-			if sell_in < 0
-			   item.quality -= 2
-			 else
-				 item.quality -= 1
-		  end
-		end
-		item.sell_in = sell_in
-	end
-	
-  def update_for_aged_brie(item) #needs refactoring
-	 qual = item.quality		
-	 if qual < 50
-	   item.quality += 1
-		 sell_in = item.sell_in - 1
-		 if sell_in < 0 && item.quality < 50
-		  item.quality += 1
-		 end			
-	 end	
-		item.sell_in = item.sell_in - 1	
-	end
-
-	def update_for_backstage(item)
-	 #for quality
-	 sell_in = item.sell_in
-   if item.quality < 50
-	   if sell_in > 10
-		  item.quality += 1
-		 elsif sell_in > 5
-			item.quality += 2
-		 elsif sell_in > 0
-			item.quality +=3													
-	   end
-	 end
-	 item.sell_in -= 1
-	 #for sell_in
-	 if item.sell_in < 0
-	   item.quality = 0
-	 end
+   items.each do |item|
+     find_updater(item)
+   end
   end
 
-	def update_for_cake(item)
-	 qual = item.quality
-	 item.sell_in = item.sell_in - 1
-	 if qual > 0
-	   if item.sell_in < 0
-		  item.quality -= 4
-		 else
-			item.quality -= 2
-		 end
-	 end		
-	end
+  def find_updater(item)
+   case item.name
+     when "Backstage passes to a TAFKAL80ETC concert"
+      update_for_backstage(item)
+     when "Aged Brie"
+      update_for_aged_brie(item)
+     when "Conjured Mana Cake"
+      update_for_cake(item)       
+     when "Sulfuras, Hand of Ragnaros"
+       #do nothing
+     else
+      do_the_common_update(item)
+   end
+  end
+
+  def do_the_common_update(item)
+   qual = item.quality
+   sell_in = item.sell_in - 1
+    if qual > 0
+      if sell_in < 0
+         item.quality -= 2
+       else
+         item.quality -= 1
+      end
+    end
+    item.sell_in = sell_in
+  end
+  
+  def update_for_aged_brie(item) #needs refactoring
+   qual = item.quality    
+   if qual < 50
+     item.quality += 1
+     sell_in = item.sell_in - 1
+     if sell_in < 0 && item.quality < 50
+      item.quality += 1
+     end      
+   end  
+    item.sell_in = item.sell_in - 1 
+  end
+
+  def update_for_backstage(item)
+   #for quality
+   sell_in = item.sell_in
+   if item.quality < 50
+     if sell_in > 10
+      item.quality += 1
+     elsif sell_in > 5
+      item.quality += 2
+     elsif sell_in > 0
+      item.quality +=3                          
+     end
+   end
+   item.sell_in -= 1
+   #for sell_in
+   if item.sell_in < 0
+     item.quality = 0
+   end
+  end
+
+  def update_for_cake(item)
+   qual = item.quality
+   item.sell_in = item.sell_in - 1
+   if qual > 0
+     if item.sell_in < 0
+      item.quality -= 4
+     else
+      item.quality -= 2
+     end
+   end    
+  end
 
 # DO NOT CHANGE THINGS BELOW -----------------------------------------
 
