3062f86f5b9efed41570fa3c2765de46a404b602
Tiffany Silveira <tsilveira@cwhen-wks-44osx.changeworks.local>
2015-04-29 14:34:16 +0100

    My refactoring

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 gem 'rspec'
 gem 'rspec-given'
+gem 'byebug', '~> 4.0.5'
--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,50 +1,82 @@
-def update_quality(items)
-  items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
-    end
-  end
-end
+	def update_quality(items)
+		items.each do |item|
+			find_updater(item)
+		end
+	end
+
+	def find_updater(item)
+		case item.name
+		when "Backstage passes to a TAFKAL80ETC concert"
+			update_for_backstage(item)
+		when "Aged Brie"
+			update_for_aged_brie(item)
+		when "Conjured Mana Cake"
+			update_for_cake(item)				
+		when "Sulfuras, Hand of Ragnaros"
+			#do nothing
+		else
+			do_the_common_update(item)
+		end
+	end
+
+	def do_the_common_update(item)
+		qual = item.quality
+		sell_in = item.sell_in - 1
+		if qual > 0
+			if sell_in < 0
+				item.quality -= 2
+			else
+				item.quality -= 1
+			end
+		end
+		item.sell_in = sell_in
+	end
+
+
+
+	def update_for_aged_brie(item) #needs refactoring
+		qual = item.quality		
+		if qual < 50
+			item.quality += 1
+			sell_in = item.sell_in - 1
+			if sell_in < 0 && item.quality < 50
+				item.quality += 1
+			end			
+		end	
+		item.sell_in = item.sell_in - 1	
+	end
+
+	def update_for_backstage(item)
+		#for quality
+		sell_in = item.sell_in
+
+		if item.quality < 50
+			if sell_in > 10
+				item.quality += 1
+			elsif sell_in > 5
+				item.quality += 2
+			elsif sell_in > 0
+				item.quality +=3													
+			end
+		end
+		item.sell_in -= 1
+		#for sell_in
+		if item.sell_in < 0
+			item.quality = 0
+		end
+	end
+
+	def update_for_cake(item)
+		qual = item.quality
+		item.sell_in = item.sell_in - 1
+		if qual > 0
+			if item.sell_in < 0
+				item.quality -= 4
+			else
+				item.quality -= 2
+			end
+		end		
+	end
 
 # DO NOT CHANGE THINGS BELOW -----------------------------------------
 
@@ -60,4 +92,7 @@ Item = Struct.new(:name, :sell_in, :quality)
 #   Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
 #   Item.new("Conjured Mana Cake", 3, 6),
 # ]
+
+
+
 
